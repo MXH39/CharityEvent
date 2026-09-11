51896bf754d31786ee4e99ea44f30b2dd811f577
root <807661486@qq.com>
2026-09-11 11:22:22 +0800

    first commit

---
+++ event_db.js
@@ -0,0 +1,16 @@
+// event_db.js
+const mysql = require('mysql2');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'charityevents_db'
+});
+db.connect((err) => {
+    if(err){
+        console.log("DB connect fail：",err);
+        return;
+    }
+    console.log("✅ charityevents_db connected");
+});
+module.exports = db;
